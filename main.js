55407e37e205b0bf34438a8af30082506b9e86fa
nenadvuk <44097253+nenadvuk@users.noreply.github.com>
2018-12-06 23:21:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,52 @@
 
-function semafor(){
-
-    boje = document.getElementsByClassName("boje");
-    boje[0].style.background = "red";
-    boje[1].style.background = "gray";
-    boje[2].style.background = "gray";
-
-function changeToYellow() {
-    boje[1].style.background = "yellow";
-
-}
-function changeToGreen() {
-    boje[0].style.background = "gray";
-    boje[1].style.background = "gray";
-    boje[2].style.background = "greenyellow";
-    
-}
+crvena = document.getElementsByClassName('boje')[0];
+zuta = document.getElementsByClassName('boje')[1];
+zelena = document.getElementsByClassName('boje')[2];
 
 
 
+function semafor() {
 
-    x = setTimeout(changeToYellow,3000);
-    y = setTimeout(changeToGreen, 5000, );
-    start = setInterval(semafor ,12000);
+    crvena.style.backgroundColor = 'gray';
+    zuta.style.backgroundColor = 'gray';
+    zelena.style.backgroundColor = 'gray';
 
+    function uCrveno() {
+        crvena.style.backgroundColor = 'red';
+        zuta.style.backgroundColor = 'gray';
+        zelena.style.backgroundColor = 'gray';
+        
+    }
+
+    function uZutoCrveno() {
+        crvena.style.backgroundColor = 'red';
+        zuta.style.backgroundColor = 'yellow';
+        zelena.style.backgroundColor = 'gray';
+    }
+
+    function uZeleno() {
+        crvena.style.backgroundColor = 'gray';
+        zuta.style.backgroundColor = 'gray';
+        zelena.style.backgroundColor = 'green';
+    }
+
+    function uZuto() {
+        crvena.style.backgroundColor = 'gray';
+        zuta.style.backgroundColor = 'yellow';
+        zelena.style.backgroundColor = 'gray';
+    }
+
+    setTimeout(uCrveno, 0);
+    setTimeout(uZutoCrveno, 4000);
+    setTimeout(uZeleno, 6500);
+    setTimeout(uZuto, 11500);
+
+    setInterval(semafor, 15000);
 }
 
 semafor();
+
+
+
+
+
